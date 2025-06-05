002d2fa1ae5f3554a06bd16dac31251aa45e8af0
victor <victor@blumteq.com>
2025-06-05 14:46:40 +0300

    creating models, controllers and migrations

---
+++ backend/app/Http/Controllers/TenantExpenseController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TenantExpense;
+use Illuminate\Http\Request;
+
+class TenantExpenseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TenantExpense $tenantExpense)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TenantExpense $tenantExpense)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TenantExpense $tenantExpense)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TenantExpense $tenantExpense)
+    {
+        //
+    }
+}
