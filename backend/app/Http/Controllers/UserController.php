4843c4adf20ac0fea9ba07d6b4261b500dcf5bd6
victor <victor@blumteq.com>
2025-06-09 17:20:50 +0300

    refactor user controller

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -13,6 +15,7 @@ class UserController extends Controller
     public function index()
     {
         //
+        return User::all();
     }
 
     /**
@@ -23,12 +26,30 @@ class UserController extends Controller
         //
     }
     
-    public function login() {
-        return ['success' => 'login success'];
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:8',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        $msg = response()->json(['error' => 'Invalid User'], 404);
+        if($user){
+            if (Hash::check($request->password, $user->password)) {
+                $authToken = $user->createToken('authToken')->plainToken;
+                $msg = response()->json(['success' => 'Login Success', 'token' => $authToken], 200);
+            }
+            else  
+                $msg = response()->json(['error' => 'Invalid Credentials'], 400);
+        }
+        return $msg; 
     }
 
-    public function logout() {
-
+    public function logout(Request $request) {
+        if (Auth::check()) {
+            $request->user()->tokens()->delete();
+            return response()->json(['success' => 'Logout Success'], 205);
+        }
+        return response()->json(['error' => 'Unauthorized Access'], 403);
     }
 
     /**
@@ -36,7 +57,22 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:8',
+            'first_name' => 'required|string',
+            'surname' => 'required|string',
+            'contact' => 'required|number',
+        ]);
         //
+        $user = new User();
+        $user->email = $request->email;
+        $user->first_name = $request->first_name;
+        $user->surname = $request->surname;
+        $user->contact = $request->contact;
+        $user->password = bcrypt($request->password);
+        $msg = $user->save() ? response()->json(['success' => 'User Registration Success'], 201): response()->json(['error' => 'User Registration Failed'], 400);
+        return $msg;
     }
 
     /**
@@ -45,6 +81,10 @@ class UserController extends Controller
     public function show(User $user)
     {
         //
+        if (!$user->id) {
+           return response()->json(['error'=> 'User Not Found'], 404);
+        }
+        return $user;
     }
 
     /**
@@ -61,6 +101,11 @@ class UserController extends Controller
     public function update(Request $request, User $user)
     {
         //
+        $msg = response()->json(['error' => 'User Not Found'], 404);
+        if ($user) {
+            $msg = $user->update($request->all()) ? response()->json(['success' => 'User Update Success'], 200) : response()->json(['error'=> 'User Update Failed']);
+        }
+        return $msg;
     }
 
     /**
@@ -69,5 +114,10 @@ class UserController extends Controller
     public function destroy(User $user)
     {
         //
+        $msg = response()->json(['error' => 'User Not Found'], 404);
+        if ($user) {
+           $msg = User::delete($user) ? response()->json(['success' => 'User Delete Success'], 200) : response()->json(['error'=> 'User Delete Failed'], 400);
+        }
+        return $msg;
     }
 }
