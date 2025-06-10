7f661da98404d1ce9e5b0df1f2cc59e74aff91b5
victor <victor@blumteq.com>
2025-06-10 17:49:14 +0300

    install bootstrap-vue-next and configuration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+import {createBootstrap} from 'bootstrap-vue-next';
+const app = createApp(App);
+app.use(createBootstrap);
+app.mount('#app');
