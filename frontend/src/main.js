c43fc2bb8e730aa65d3709424aa45fa25f1ce724
victor <victor@blumteq.com>
2025-06-11 14:07:45 +0300

    refactor scripts

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
-import {createBootstrap} from 'bootstrap-vue-next';
 const app = createApp(App);
-app.use(createBootstrap);
 app.mount('#app');
